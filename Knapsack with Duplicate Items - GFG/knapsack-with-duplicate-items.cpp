d916a9402641eb27b2e424d189279de85689db32
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2023-10-25 00:50:36 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,19 +9,27 @@ using namespace std;
 
 class Solution{
 public:
-    int knapSack(int N, int W, int val[], int wt[])
-    {
-        // code here
-         vector<int>dp(W+1,0);
-        for(int i=0;i<=W;i++){
-            for(int j=0;j<N;j++){
-                if(i-wt[j]>=0)
-                    dp[i]=max(dp[i],dp[i-wt[j]]+val[j]);
+    int knapSack(int N, int W, int val[], int wt[]) {
+        // Initialize a dynamic programming (DP) array to store the maximum value for each weight.
+        vector<int> dp(W + 1, 0);
+
+        // Iterate through each possible weight from 0 to W.
+        for (int i = 0; i <= W; i++) {
+            // Iterate through each item (N items) to consider adding them to the knapsack.
+            for (int j = 0; j < N; j++) {
+                // Check if it's possible to add the j-th item without exceeding the current weight limit (i).
+                if (i - wt[j] >= 0) {
+                    // Update the DP array at weight (i) by choosing the maximum value between the current value
+                    // (dp[i]) and the value of adding the j-th item (val[j]) to the maximum value obtained with the
+                    // remaining weight (i - wt[j]).
+                    dp[i] = max(dp[i], dp[i - wt[j]] + val[j]);
+                }
             }
         }
+
+        // The final value in the DP array at weight (W) represents the maximum value achievable within the given weight limit.
         return dp[W];
     }
-    
 };
 
 //{ Driver Code Starts.
